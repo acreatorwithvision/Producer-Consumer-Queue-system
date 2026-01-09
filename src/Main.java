f2beceecad811236b55fa94794d6d1c7298a322a
acreatorwithvision <fake49403@gmail.com>
2026-01-10 00:47:11 +0530

    Implement graceful shutdown of Main Thread.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         BlockingQueue<Order> kitchenCounter=new ArrayBlockingQueue<>(5);
         KitchenMetrics metrics=new KitchenMetrics();
 
-        //Start 1 order taker
-        new Thread(new Producer(kitchenCounter)).start();
-        new Thread(new Consumer(kitchenCounter, "Chef Mario", metrics)).start();
-        new Thread(new Consumer(kitchenCounter, "Chef Luigi",metrics)).start();
+        ExecutorService executor=Executors.newFixedThreadPool(3);
 
+        Producer orderTaker=new Producer(kitchenCounter);
+        executor.execute(orderTaker);
+        executor.execute(new Consumer(kitchenCounter, "Chef Mario",metrics));
+        executor.execute(new Consumer(kitchenCounter, "chef Luigi", metrics));
+
+        Thread.sleep(5000);
+
+        System.out.println(">>>> CLOSING TIME <<<<");
+        orderTaker.stop();
+        executor.shutdown();
+
+        if(executor.awaitTermination(10, TimeUnit.SECONDS)){
+            System.out.println("All Chefs finished. Kitchen clean");
+        }
+        else{
+            System.out.println("Forcing Closure");
+            executor.shutdownNow();
+        }
+        metrics.printStats();
     }
 }
--- src/Producer.java
+++ src/Producer.java
@@ -3,18 +3,22 @@ import java.util.concurrent.BlockingQueue;
 public class Producer implements Runnable {
     private final BlockingQueue<Order> queue;
     private int orderIdCounter = 1;
+    private volatile boolean running=true;
 
     public Producer(BlockingQueue<Order> queue) {
         this.queue = queue;
     }
 
+    public void stop(){ running = false;}
+
     @Override
     public void run() {
         try {
-            while (true) {
+            while (running) {
                 queue.put(new Order(orderIdCounter++)); // Changed to .put()
-                Thread.sleep(800); 
+                Thread.sleep(500); 
             }
+            System.out.println("Order Taker: Shop is closed, no more orders.");
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
